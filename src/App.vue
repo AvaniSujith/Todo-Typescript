01f0799ecb27bf5b1f192ad3ce73fdbf81da5cd2
AvaniSujith <chinchuavani123@gmail.com>
2025-06-24 14:08:07 +0530

    feat: convert input bar into typed version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+import InputBar from "./components/InputBar.vue";
+
+const inputValue = ref("");
 </script>
 
 <template>
-
+  <InputBar placeholder="Search" v-model="inputValue" />
+  <p>{{ inputValue }}</p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/InputBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: string;
+  placeholder: string;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+const handleInput = (event: Event) => {
+    const inputValue = (event.target as HTMLInputElement).value
+    emit("update:modelValue", inputValue);
+}
+
+</script>
+
+<template>
+  <input
+    class="input-bar"
+    type="text"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="handleInput"
+  />
+</template>
+
+<style scoped>
+.input-bar {
+  border: 2px solid #eee;
+  padding: 8px;
+  border-radius: 8px;
+  font-size: 18px;
+  margin: 10px 0px;
+  width: 100%;
+}
+</style>
