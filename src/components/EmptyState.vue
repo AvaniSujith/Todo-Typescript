bf51e213bf2efb0daa81d3ffbab3db74e648b67b
AvaniSujith <chinchuavani123@gmail.com>
2025-06-25 12:31:20 +0530

    feat: Update empty state component using typescript

---
+++ src/components/EmptyState.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+    message: String;
+    }>();
+
+const emit = defineEmits<{
+    (e: 'exit', message: string) : void
+}>();
+
+const handleBackButton = () =>{
+    emit('exit', 'back button clicked')
+}
+
+</script>
+
+<template>
+    <div>
+        <p class="emtpy-label">{{ props.message }}</p>
+        <button class="empty-btn" @click="handleBackButton">Go Back</button>
+    </div>
+</template>
+
+<style scoped></style>
