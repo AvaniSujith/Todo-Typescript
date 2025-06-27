d3ecb08e51f5d5892e370d58b2743403fa9fb6e1
AvaniSujith <chinchuavani123@gmail.com>
2025-06-27 11:23:01 +0530

    refactor: simplify logic - 1

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,25 +1,39 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 interface Task {
   id: number;
   title: string;
   completed: boolean;
-  isEdit: boolean;
-  newTitle: string;
 }
 
-defineProps<{
-  tasks: Task[];
-}>();
+// defineProps<{
+//   tasks: Task[];
+// }>();
+
+const tasks = ref<Task[]>([
+  {
+    id: 1,
+    title: "task1",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "task2",
+    completed: false,
+  },
+]);
+
+const tempId = ref<number>();
+const tempTitle = ref<string>('');
 
 const emit = defineEmits<{
   (e: "updateTask", id: number): void;
   (e: "deleteTask", id: number): void;
   (
     e: "saveTask",
-    id: number,
-    title: string,
-    isEdit: boolean,
-    completed: boolean
+
+    id: number
   ): void;
   (e: "editTask", id: number): void;
 }>();
@@ -32,56 +46,47 @@ const handleDelete = (id: number) => {
   emit("deleteTask", id);
 };
 
-const handleSave = (
-  id: number,
-  title: string,
-  isEdit: boolean,
-  completed: boolean
-) => {
-  emit("saveTask", id, title, isEdit, completed);
-};
+// const handleSave = (id: number) => {
+//   emit("saveTask", id);
+// };
 
-function handleEdit(id: number) {
-  emit("editTask", id);
+function handleEdit(id: number, title: string) {
+  tempId.value = id;
+  tempTitle.value = title;
+  //   console.log(tempId.value, tempTitle.value)
+}
+
+function saveEdit(id: number) {
+  const task = tasks.value.find((task) => task.id === id);
+  if (task && tempTitle) {
+    task.title = tempTitle.value;
+    tempId.value = undefined;
+    tempTitle.value = '';
+  }
 }
 </script>
 
 <template>
   <TransitionGroup name="list" tag="ul" class="tasks">
     <li class="task-item" v-for="task in tasks" :key="task.id">
-      <div class="task-detail" v-if="!task.isEdit">
-        <input
-          type="checkbox"
-          :checked="task.completed"
-          @change="handleUpdate(task.id)"
-        />
-        <p class="task-label">{{ task.title }}</p>
-        <button class="edit-btn" @click="handleEdit(task.id)">Edit</button>
-      </div>
-      <div class="task-detail" v-else>
-        <input
-          type="checkbox"
-          :checked="task.completed"
-          @change="handleUpdate(task.id)"
-        />
-        <input type="text" v-model="task.newTitle" />
-        <button
-          class="edit-btn"
-          @click="
-            handleSave(task.id, task.newTitle, task.isEdit, task.completed)
-          "
-        >
-          Save
-        </button>
-      </div>
+      <input
+        type="checkbox"
+        :checked="task.completed"
+        @change="handleUpdate(task.id)"
+      />
+      <p class="task-label" v-if="tempId !== task.id">{{ task.title }}</p>
+      <input type="text" v-else v-model="tempTitle" />
+
       <div class="task-buttons">
         <button
-          class="del-btn"
-          @click="handleDelete(task.id)"
-          :disabled="task.isEdit"
+          class="edit-btn"
+          v-if="tempId !== task.id"
+          @click="handleEdit(task.id, task.title)"
         >
-          Delete
+          Edit
         </button>
+        <button class="save-btn" v-else @click="saveEdit(tempId)">Save</button>
+        <button class="del-btn" @click="handleDelete(task.id)">Delete</button>
       </div>
     </li>
   </TransitionGroup>
