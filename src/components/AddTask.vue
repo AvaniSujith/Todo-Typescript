582e7f2d105cdaa89bcd8770a0857984bf892adb
AvaniSujith <chinchuavani123@gmail.com>
2025-06-28 11:20:55 +0530

    add task file

---
+++ src/components/AddTask.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { useTaskStore } from '../store/Task';
+
+import InputBar from './InputBar.vue';
+
+const taskStore = useTaskStore();
+
+const taskTitle = ref<string>('');
+
+const handleAddTask = (taskTitle: string) => {
+    taskStore.addTask(taskTitle);
+}
+
+</script>
+<template>
+
+    <input-bar placeholder="Add Todo" v-model="taskTitle" @keyup.enter="handleAddTask"/>
+    <button class="add-btn" @click="handleAddTask(taskTitle)">Add</button>
+
+</template>
+<style>
+</style>
