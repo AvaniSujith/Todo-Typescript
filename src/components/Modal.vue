bd85ea882c85395968d72a35c8078bd5bc7b7dc2
AvaniSujith <chinchuavani123@gmail.com>
2025-07-04 11:53:48 +0530

    feat: inital setup

---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+    content: string;
+}>()
+</script>
+
+<template>
+    <div class="modal-container">
+        <div class="modal">
+            <p>
+            {{ content }}
+        </p>
+        <div class="buttons-container">
+            <button class="delete-btn">Delete</button>
+            <button class="cancel-btn">Cancel</button>
+        </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal-container{
+    position: fixed;
+    z-index: 99999;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
